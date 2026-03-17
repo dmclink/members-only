deb5c5515ba46f3e5f3ea312fb34c84ae966d327
dmclink <135415819+dmclink@users.noreply.github.com>
2026-03-17 20:08:43 +0800

    feat: seed admin priv user to database

--- src/db/init.js
+++ src/db/init.js
@@ -1,26 +1,37 @@
 const db = require('../config/db.js');
+const dbQueries = require('../db/queries.js');
 console.log('initializing db');
 
-db.query(
-	`
-CREATE TABLE IF NOT EXISTS users (
-	id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	username VARCHAR(255) UNIQUE,
-	hashed_password VARCHAR(255),
-	salt VARCHAR(255),
-	club_code VARCHAR(255),
-	admin BOOL
-)`,
-)
-	.then(
-		db.query(
-			`
-CREATE TABLE IF NOT EXISTS messages (
-id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-user_id INTEGER REFERENCES users(id),
-message VARCHAR(255),
-timestamp TIMESTAMP
-)`,
-		),
-	)
-	.catch((err) => console.error(err));
+async function main() {
+	try {
+		await db.query(`
+			CREATE TABLE IF NOT EXISTS users (
+				id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+				username VARCHAR(255) UNIQUE,
+				hashed_password VARCHAR(255),
+				salt VARCHAR(255),
+				club_code VARCHAR(255),
+				admin BOOL
+			)`);
+		await db.query(`
+			CREATE TABLE IF NOT EXISTS messages (
+				id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+				user_id INTEGER REFERENCES users(id),
+				message VARCHAR(255),
+				timestamp TIMESTAMP
+			)`);
+
+		const result = await dbQueries.usernameAlreadyExists('dmclink');
+		const adminExists = result.rows[0].user_exists;
+
+		if (adminExists) {
+			await db.query(`
+				UPDATE users SET admin = true WHERE username = 'dmclink'
+			`);
+		}
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+main();
